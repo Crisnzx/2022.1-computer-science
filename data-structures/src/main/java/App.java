358b5499b19fce57f8e0cc9db8c6186f5fe9dccc
Crisnzx <cristovaon13@gmail.com>
2022-10-13 14:29:33 -0300

    feat: Add maven

--- data-structures/src/main/java/App.java
+++ data-structures/src/main/java/App.java
@@ -1,6 +1,6 @@
 import sort.SortableArray;
 
-public class Main {
+public class App {
   public static void main(String[] args) {
     int[] array = new int[] { 5, 1, 67, 3, 54, 12, 23, 4 };
     SortableArray sortableArray = new SortableArray(array);
