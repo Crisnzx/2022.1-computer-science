fe2063ba7e108f111a89cf3da190f13abb5594e2
121110199 CRISTOVAO PESSOA CANDIDO NETO <cristovao.candido.neto@lcc2-43.lcc.ufcg.edu.br>
2022-10-25 11:25:56 -0300

    perf: R03 - Improve CountingSort by getting the smallest and greatest elements of the array

--- eda-leda/leda-roteiros/R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/Main.java
+++ eda-leda/leda-roteiros/R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/Main.java
@@ -4,13 +4,19 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        CountingSort countingSort = new CountingSort();
+        ExtendedCountingSort countingSort = new ExtendedCountingSort();
         Integer[] array = new Integer[100];
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = new Random().nextInt(1000);
+            array[i] = new Random().nextInt(1000) + 5000000;
         }
-
+        long timeBeforeSort = System.currentTimeMillis();
         countingSort.sort(array);
+        long timeAfterSort = System.currentTimeMillis();
+
+        long executionTime = timeAfterSort - timeBeforeSort;
+        System.out.println(timeBeforeSort);
+        System.out.println(timeAfterSort);
+        System.out.println("Tempo de execucão: " + executionTime);
     }
 }
