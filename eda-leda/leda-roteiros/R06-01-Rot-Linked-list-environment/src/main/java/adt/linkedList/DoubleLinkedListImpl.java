628e800edbe49fb6cdfdbed7131fc28086f06a85
Crisnzx <cristovaon13@gmail.com>
2022-11-23 10:28:29 -0300

    feat: Implement DoubleLinkedListImpl - R06

--- eda-leda/leda-roteiros/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
+++ eda-leda/leda-roteiros/R06-01-Rot-Linked-list-environment/src/main/java/adt/linkedList/DoubleLinkedListImpl.java
@@ -5,22 +5,67 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 
 	protected DoubleLinkedListNode<T> last;
 
+	public DoubleLinkedListImpl() {
+		DoubleLinkedListNode<T> firstNode = new DoubleLinkedListNode<T>();
+		DoubleLinkedListNode<T> lastNode = new DoubleLinkedListNode<T>();
+
+		firstNode.setNext(lastNode);
+		lastNode.setPrevious(firstNode);
+
+		this.head = firstNode;
+		this.last = lastNode;
+
+	}
+
+	@Override
+	public void insert(T element) {
+		DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(element, this.last, this.last.previous);
+
+		this.last.previous.next = newNode;
+		this.last.previous = newNode;
+	}
+
+	@Override
+	public void remove(T element) {
+		DoubleLinkedListNode<T> current = this.last;
+		while (!current.previous.isNIL()) {
+			if (current.previous.getData().equals(element)) {
+				current.previous = current.previous.previous;
+				current.previous.next = current;
+				return;
+			}
+			current = current.previous;
+		}
+	}
+
 	@Override
 	public void insertFirst(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		DoubleLinkedListNode<T> firstNode = (DoubleLinkedListNode<T>) this.head;
+		DoubleLinkedListNode<T> newNode = new DoubleLinkedListNode<T>(element, (DoubleLinkedListNode<T>) firstNode.next,
+				firstNode);
+		((DoubleLinkedListNode<T>) firstNode.next).previous = newNode;
+		firstNode.next = newNode;
 	}
 
 	@Override
 	public void removeFirst() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.isEmpty()) {
+			return;
+		}
+		DoubleLinkedListNode<T> firstNode = (DoubleLinkedListNode<T>) this.head;
+		((DoubleLinkedListNode<T>) firstNode.next.next).previous = firstNode;
+		firstNode.next = firstNode.next.next;
 	}
 
 	@Override
 	public void removeLast() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.isEmpty()) {
+			return;
+		}
+
+		this.last.previous.previous.next = this.last;
+		this.last.previous = this.last.previous.previous;
+
 	}
 
 	public DoubleLinkedListNode<T> getLast() {
