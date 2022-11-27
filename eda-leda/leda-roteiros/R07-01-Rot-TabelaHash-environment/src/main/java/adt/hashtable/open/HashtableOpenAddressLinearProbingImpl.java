fb64a0ad9c482611bd25448006c6cc78c3ad04c5
Crisnzx <cristovaon13@gmail.com>
2022-11-26 23:12:45 -0300

    feat: Add Open Address Hashtable implementations - R07

--- eda-leda/leda-roteiros/R07-01-Rot-TabelaHash-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ eda-leda/leda-roteiros/R07-01-Rot-TabelaHash-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -17,29 +17,49 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 	public void insert(T element) {
 		for (int i = 0; i < this.table.length; i++) {
 			int elementHash = ((HashFunctionLinearProbing) this.hashFunction).hash(element, i);
-			if (this.table[elementHash] != null || this.table[elementHash].equals(DELETED)) {
-				// adiciona, se n passa adiante
+			if (this.table[elementHash] == null || this.table[elementHash].equals(element) ||
+					this.table[elementHash].equals(this.deletedElement)) {
+				this.table[elementHash] = element;
+				this.elements += 1;
+				return;
 			}
+			this.COLLISIONS += 1;
 
 		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (int i = 0; i < this.table.length; i++) {
+			int elementHash = ((HashFunctionLinearProbing) this.hashFunction).hash(element, i);
+			if (this.table[elementHash] != null && this.table[elementHash].equals(element)) {
+				this.table[elementHash] = this.deletedElement;
+				this.elements -= 1;
+				return;
+			}
+		}
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (int i = 0; i < this.table.length; i++) {
+			int elementHash = ((HashFunctionLinearProbing) this.hashFunction).hash(element, i);
+			if (this.table[elementHash] != null && this.table[elementHash].equals(element)) {
+				return (T) this.table[elementHash];
+			}
+		}
+		return null;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (int i = 0; i < this.table.length; i++) {
+			int elementHash = ((HashFunctionLinearProbing) this.hashFunction).hash(element, i);
+			if (this.table[elementHash] != null && this.table[elementHash].equals(element)) {
+				return elementHash;
+			}
+		}
+		return -1;
 	}
 
 }
