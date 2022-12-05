bab86573aeca19cf4f2551595f95887b93370683
Crisnzx <cristovaon13@gmail.com>
2022-12-05 09:54:33 -0300

    feat: Add equals - R08

--- eda-leda/leda-roteiros/R08-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ eda-leda/leda-roteiros/R08-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -1,7 +1,8 @@
 package adt.bst;
 
 /**
- * - Esta eh a unica classe que pode ser modificada 
+ * - Esta eh a unica classe que pode ser modificada
+ * 
  * @author adalbertocajueiro
  *
  * @param <T>
@@ -10,8 +11,16 @@ public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements Simpl
 
 	@Override
 	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return this.equals((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+	}
+
+	private boolean equals(BSTNode<T> node1, BSTNode<T> node2) {
+		if (!node1.equals(node2)) {
+			return false;
+		}
+
+		return this.equals((BSTNode<T>) node1.getLeft(), (BSTNode<T>) node2.getRight())
+				&& this.equals((BSTNode<T>) node1.getRight(), (BSTNode<T>) node2.getRight());
 	}
 
 	@Override
