d72ecd962a359eae38bab5e9c266fae456bb5fb8
Crisnzx <cristovaon13@gmail.com>
2022-12-11 14:01:26 -0300

    feat: Add order statistics implementation - R09

--- eda-leda/leda-roteiros/R09-01-Rot-HeapBinaria-environment/src/main/java/orderStatistic/OrderStatisticsHeapImpl.java
+++ eda-leda/leda-roteiros/R09-01-Rot-HeapBinaria-environment/src/main/java/orderStatistic/OrderStatisticsHeapImpl.java
@@ -5,24 +5,40 @@ import java.util.PriorityQueue;
 public class OrderStatisticsHeapImpl<T extends Comparable<T>> implements OrderStatistics<T> {
 
 	/**
-	 * Existem diversas formas de se calcular uma estatistica de ordem. 
+	 * Existem diversas formas de se calcular uma estatistica de ordem.
 	 * Voce deve fazer isso usando uma heap restricoes:
 	 * - nenhuma copia ou estrutura array auxiliar serah permitida, exceto o uso de
-	 *   uma PriorityQueue
-	 * - caso a estatistica de ordem procurada nao exista no array o metodo deve retornar null 
+	 * uma PriorityQueue
+	 * - caso a estatistica de ordem procurada nao exista no array o metodo deve
+	 * retornar null
 	 * 
 	 * @param array
 	 * @param k
 	 * @return
 	 */
-	
+
 	@Override
 	public T getOrderStatistics(T[] array, int k) {
+
+		if (k < 1 || k > array.length) {
+			return null;
+		}
+
 		PriorityQueue<T> heap = new PriorityQueue<T>();
-		//TODO IMplement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		for (int i = 0; i < array.length; i++) {
+			heap.add(array[i]);
+		}
+
+		for (int i = 0; i < k - 1; i++) {
+			T removedElement = heap.poll();
+			if (removedElement == null) {
+				return null;
+			}
+		}
+
+		return heap.peek();
+
 	}
 
-	
-	
 }
